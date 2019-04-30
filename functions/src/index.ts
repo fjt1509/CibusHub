4cf6987e57cfd3979f9385995c9dc3c63e6df8b8
kENT <pedersenkentjuul@gmail.com>
2019-04-30 12:10:20 +0200

    first function ok

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,10 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello From Firebase");
+admin.initializeApp()
+exports.posts = functions.https.onRequest((request, response) => {
+  admin.firestore().collection('post').get().then(posts => {
+    console.log(posts)
+  }).catch(err=> {console.log(err)})
+
 });

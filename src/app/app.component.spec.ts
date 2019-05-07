efc27dc972b7219e26fcc75556e2bd48ae42443d
kENT <pedersenkentjuul@gmail.com>
2019-05-07 14:05:24 +0200

    unit test NOT DONE

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,34 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {MzNavbarModule} from 'ngx-materialize';
+import {NavbarModule} from './navbar/navbar.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MzNavbarModule,
+        NavbarModule,
+        BrowserModule,
+        AppRoutingModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+        AngularFireStorageModule,
+        AngularFireAuthModule,
+        BrowserAnimationsModule,
+        NavbarModule,
+        HttpClientModule
       ],
       declarations: [
         AppComponent
--- src/app/files/shared/file.service.spec.ts
+++ src/app/files/shared/file.service.spec.ts
@@ -1,8 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FileService } from './file.service';
+import {AngularFirestore, AngularFirestoreModule} from '@angular/fire/firestore';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('FileService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let fireStoreMock: any;
+  beforeEach(() => {
+    fireStoreMock = jasmine.createSpyObj('AngularFirestore', ['collection'])
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFirestoreModule
+      ],
+      providers: [
+        {provide: AngularFirestore, useValue: fireStoreMock}
+      ]
+    });
+  });
 
 });
